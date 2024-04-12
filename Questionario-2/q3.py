db2c523186b9231332637d0395395cf203fb5096
herculesnatan <medeiroshercules23@gmail.com>
2024-04-12 16:44:51 -0300

    [Questionário] Entrada/Saída de Dados

---
+++ Questionario-2/q3.py
@@ -0,0 +1,6 @@
+sabor_1, sabor_2, sabor_3 = input().split()
+cobertura_1, cobertura_2 = input().split(",")
+sabor_da_calda = input()
+
+print("Sorvete de %s, %s e %s com coberturas de %s e %s e calda de %s!" %(sabor_1, sabor_2, sabor_3, cobertura_1, cobertura_2, sabor_da_calda))
+print("Não esqueça a banana!")
---
+++ Questionario-2/q6.py
@@ -0,0 +1,5 @@
+num = float(input())
+
+print("%.4f" %(num))
+print("%.2f" %(num))
+print("%.0f" %(num))
